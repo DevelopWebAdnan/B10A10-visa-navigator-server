8127fee2a26e2f5bd093d63d9c6768b3a2e3eac1
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2025-12-24 11:16:08 +0600

    Displayed users in a list in an extra section in the Home page; created GET and DELETE user apis.

--- index.js
+++ index.js
@@ -79,11 +79,23 @@ async function run() {
     })
 
     // Users related apis
+    app.get('/users', async(req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
     app.post('/users', async(req, res) => {
       const newUser = req.body;
       console.log('New user created: ', newUser);
       const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    })
+
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     })
 
